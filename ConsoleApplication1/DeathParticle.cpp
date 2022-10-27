a791791d3d746d5f1d415634bd008b2b1850f0be
Geegaz <geegazrd@gmail.com>
2022-10-27 21:37:32 +0200

    First iteration of DeathParticle

--- ConsoleApplication1/DeathParticle.cpp
+++ ConsoleApplication1/DeathParticle.cpp
@@ -1,6 +1,22 @@
 #include "DeathParticle.h"
 
-idDeathParticle::idDeathParticle(int _x, int _y, int _lifespan)
-	:position(x, y), lifespan(_lifespan), life(0) {
+extern float delta_time;
 
+idDeathParticle::idDeathParticle(int x, int y, int _lifespan, idSprite& _sprite)
+	:idParticle(x, y, _lifespan), sprite(_sprite)
+{
+	float angle = ((float)rand() / (RAND_MAX)) * PI;
+	frame = rand()% (sprite.frames.x * sprite.frames.y);
+	velocity = floatVector2_t(
+		cosf(angle),
+		sinf(angle)
+	) * 20.0f;
+}
+
+void idDeathParticle::Update() {
+	position += velocity * delta_time;
+}
+
+void idDeathParticle::Draw(idDisplay& display) {
+	sprite.Draw(display, position.x, position.y, frame);
 }
--- ConsoleApplication1/DeathParticle.h
+++ ConsoleApplication1/DeathParticle.h
@@ -1,14 +1,18 @@
 #pragma once
 #include "Particle.h"
+#include "Sprite.h"
+#include <random>
+#define PI 3.141592
 
 class idDeathParticle :
     public idParticle
 {
 private:
     floatVector2_t velocity;
+    idSprite& sprite;
     int frame;
 public:
-    idDeathParticle(int x, int y, int _lifespan);
+    idDeathParticle(int x, int y, int _lifespan, idSprite& _sprite);
     void Update();
     void Draw(idDisplay& display);
 };
